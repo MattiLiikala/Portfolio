53ed7801259e0c8df71c6908c23e4bd4a0072779
Matti Liikala <mamilii@utu.fi>
2016-01-28 10:09:27 +0200

    re-scaling

--- client/scripts/about.js
+++ client/scripts/about.js
@@ -4,14 +4,13 @@ var MainComponent = React.createClass({
       <div className="mainContainer">
       <h1> Contact Me! </h1>
         <h2>Email:
-        <a href="mailto:mamilii@utu.fi" target="_top">mamilii@utu.fi</a>
-        <br></br>
-        Phone: +358 409642995
-        </h2>
+        <a href="mailto:mamilii@utu.fi" target="_top">mamilii@utu.fi</a></h2>
+        <h2>
+        Phone: +358 409642995</h2>
       </div>
     );
   }
-}); 
+});
 
 ReactDOM.render(
   <MainComponent/>,
