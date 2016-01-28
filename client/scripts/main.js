6078dd2b2de0dbfce3214fbb6f5ef27d3eb1b9d3
Matti Liikala <mamilii@utu.fi>
2016-01-28 11:03:26 +0200

    profile picture added

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -2,6 +2,7 @@ var MainComponent = React.createClass({
   render: function(){
     return(
       <div className="mainContainer">
+      <img src="images/profile.jpg"></img>
       <h1> Matti <span className="surname">Liikala</span> </h1>
         <p>I am an IT -student from University of Turku. For
         a couple of years I have been doing lots of Graphics for
