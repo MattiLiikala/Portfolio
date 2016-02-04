10dd5c70070ccc10c5bd14398bf52818d5bf3999
Matti Liikala <mamilii@utu.fi>
2016-02-04 11:55:58 +0200

    Typofix

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -4,7 +4,7 @@ var MainComponent = React.createClass({
       <div className="mainContainer">
       <img src="images/profile.jpg"></img>
       <h1> Matti <span className="surname">Liikala</span> </h1>
-        <p>I am an IT -student from University of Turku. For
+        <p>I am an IT student from University of Turku. For
         a couple of years I have been doing lots of graphics for
         various student events and different projects. My main
         focus is on graphics and UX design.</p>
