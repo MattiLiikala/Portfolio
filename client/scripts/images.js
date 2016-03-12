21eaed446c7b240beb04fdfd5a69ff316f09de06
Matti Liikala <mamilii@utu.fi>
2016-03-12 19:23:45 +0200

    googlePlay -link added

--- client/scripts/images.js
+++ client/scripts/images.js
@@ -31,6 +31,8 @@ var MainComponent = React.createClass({
       		<a class="game2-image-link" href="images/HS_screencap3.jpg" data-lightbox="Hamster Shelter" data-title="Gameplay"><img class="Game2" src="images/HS_screencap3_thumb.jpg" alt=""/></a>
           <a class="game2-image-link" href="images/HS_screencap4.jpg" data-lightbox="Hamster Shelter" data-title="Gameplay"><img class="Game2" src="images/HS_screencap4_thumb.jpg" alt=""/></a>
 
+        <a className="playStore" href="https://play.google.com/store/apps/details?id=com.HamsterGames.HamsterShelter&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"><img alt="Get it on Google Play" src="https://play.google.com/intl/en_us/badges/images/generic/en-play-badge.png" /></a>
+
         <h2>Logos</h2>
         <p1></p1>
         	<a class="logo-image-link" href="images/UTGS_logo.png" data-lightbox="Logos" data-title="Under The Great Sky"><img class="logo" src="images/UTGS_logo_thumb.png" alt=""/></a>
